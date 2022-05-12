85a265ad812022ad29bbfce0609c04e1a829c9e1
RuslanPalytin <life_paradishe@mail.ru>
2022-05-12 18:08:01 +0300

    Добавьте файлы проекта.

---
+++ Сессия__1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Сессия__1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void update(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Cancel(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Vxod(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
